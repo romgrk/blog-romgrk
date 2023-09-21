8b5c6abf53b1ba309b4b89526128a92759abce9d
Rom Grk <romgrk.cc@gmail.com>
2023-09-20 21:59:51 -0400

    update

--- src/components/client/RandomPlant/RandomPlant.tsx
+++ src/components/client/RandomPlant/RandomPlant.tsx
@@ -4,8 +4,6 @@ import plant2 from './plant-2.svg?raw'
 
 import './RandomPlant.css'
 
-/* Image by <a href="https://www.freepik.com/free-vector/houseplant-collection-illustration_13558893.htm#query=plant%20svg&position=19&from_view=keyword&track=ais">Freepik</a> */
-
 const plants = [
   plant1,
   plant2,
