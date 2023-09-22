0db6824bd7e2ebc81c62154230f89f619a81ff44
Rom Grk <romgrk.cc@gmail.com>
2023-09-21 23:15:58 -0400

    update

--- src/components/client/posts/react-function-components.tsx
+++ src/components/client/posts/react-function-components.tsx
@@ -1,12 +1,5 @@
 import CodeRunner from '../CodeRunner'
 
-function transformOutput(output: string) {
-  const lines = output.split('\n')
-  return lines.slice(1, 6).join('\n')
-    .replaceAll(/^\s+at eval/gm, '<anonymous>')
-    .replaceAll(/^\s+at /gm, '')
-}
-
 export function ExampleStack() {
   return (
     <div className='row mb-1'>
@@ -52,6 +45,128 @@ export function ExampleStack() {
 
           Component()()
         `}
+      />
+
+    </div>
+  )
+}
+
+function transformOutput(output: string) {
+  const lines = output.split('\n')
+  return lines.slice(0, 6).join('\n')
+    .replace(/Error\s+/m, '')
+    .replaceAll(/^\s*(at )?eval/gm, '<anonymous>')
+    .replaceAll(/^\s*(at )?/gm, '')
+}
+
+
+const CONTEXT = `
+function cuid() { return 42 }
+function createRef() { return { current: null } }
+
+let currentContext = null
+const contexts = []
+
+function nextContext() {
+  if (!currentContext.next) {
+    currentContext.next = { value: null, next: null }
+  }
+  return (currentContext = currentContext.next)
+}
+
+function useRef() {
+  nextContext()
+  if (currentContext.value)
+    return currentContext.value
+  return (currentContext.value = { current: null })
+}
+function useState(s) {
+  nextContext()
+  if (currentContext.value)
+    return [currentContext.value[0], currentContext.value[1]]
+  return (currentContext.value = [s, () => {}])
+}
+function useCallback(fn, d) {
+  nextContext()
+  if (currentContext.value)
+    return currentContext.value
+  return (currentContext.value = fn)
+}
+`
+
+const CLASS_VERSION = `
+  class Selector {
+    id = cuid()
+    ref = createRef()
+    position = { x: 0, y: 0}
+
+    state = {
+      open: false,
+      loading: false,
+      disabled: false,
+      selectedIndex: -1,
+    }
+
+    open() {}
+    close() {}
+    select(index) {}
+    focus() {}
+
+    render() {
+      return '<div>Hello</div>'
+    }
+  }
+
+  const start = Date.now()
+  const elements = Array.from({ length: 1000 }).map(() => new Selector())
+  for (let i = 0; i < 100; i++) {
+    elements.forEach(e => e.render())
+  }
+  const end = Date.now()
+  console.log((end - start)/100 + 'ms')
+`
+
+const FUNCTION_VERSION = `
+  function Selector(props) {
+    const id = useRef(cuid())
+    const ref = useRef()
+    const position = useRef({ x: 0, y: 0 })
+
+    const [isOpen, setOpen] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [disabled, setDisabled] = useState(false)
+    const [selectedIndex, setSelectedIndex] = useState(-1)
+
+    const open = useCallback(() => {}, [])
+    const close = useCallback(() => {}, [])
+    const select = useCallback((index) => {}, [])
+    const focus = useCallback(() => {}, [])
+
+    return '<div>Hello</div>'
+  }
+
+  const start = Date.now()
+  const elements = Array.from({ length: 1000 }).map((_, i) => (contexts[i] = { value: null, next: null }))
+  for (let i = 0; i < 100; i++) {
+    elements.forEach((_, i) => (currentContext = contexts[i], Selector()))
+  }
+  const end = Date.now()
+  console.log((end - start)/100 + 'ms')
+`
+
+
+export function ExamplePerformance() {
+  return (
+    <div className='row mb-1'>
+
+      <CodeRunner
+        context={CONTEXT}
+        code={CLASS_VERSION}
+      />
+
+      <CodeRunner
+        context={CONTEXT}
+        code={FUNCTION_VERSION}
       />
 
     </div>
