2bb0a6c752a193978a885394d3e758255bd8848b
Rom Grk <romgrk.cc@gmail.com>
2023-09-22 13:35:19 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://romgrk.com',
   integrations: [mdx(), sitemap(), react()],
   markdown: {
     shikiConfig: {
