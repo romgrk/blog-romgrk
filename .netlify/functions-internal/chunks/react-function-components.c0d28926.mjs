85c7acc0a5e7b20f08e24b4a79b0aa4b22c3bcfd
Rom Grk <romgrk.cc@gmail.com>
2023-09-22 14:28:41 -0400

    update

--- .netlify/functions-internal/chunks/react-function-components.c0d28926.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./react-function-components.5f03ac72.mjs');
-					}
-					const collectedLinks = ["/_astro/CodeRunner.b5ac2ea5.css","/_astro/RandomPlant.b39ee131.css"];
-					const collectedStyles = "@@ASTRO-STYLES@@";
-					const collectedScripts = "@@ASTRO-SCRIPTS@@";
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
