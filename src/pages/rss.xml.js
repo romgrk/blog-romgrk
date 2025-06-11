bcccfbe822e6d2480a4ae665de7ddd376ee872ba
Rom Grk <romgrk.cc@gmail.com>
2025-06-10 20:21:08 -0400

    fix: rss.xml

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,14 +2,14 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
-export async function get(context) {
+export async function GET(context) {
   const posts = await getCollection('posts');
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
     items: posts
-      .filter(post => !post.draft)
+      .filter((post) => !post.draft)
       .map((post) => ({ ...post.data, link: `/posts/${post.slug}/` })),
   });
 }
